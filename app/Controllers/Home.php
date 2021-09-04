826b5065fde0532d68d19db607d372c3d2a2e9ff
venkat nikhil <vnikhil955@gmail.com>
2021-09-04 13:16:45 +0530

    method of fetching data has changed

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,24 +2,42 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\HTTP\Response;
+
 class Home extends BaseController
 {
 	public function index()
 	{
-		$contact_name = $_POST['name'];
-		$contact_email = $_POST['email'];
-		$contact_message = $_POST['message'];
 
-		$contact = [
-			'contact_name' => $contact_name,
-			'contact_email' => $contact_email,
-			'contact_message' => $contact_message
-		];
+		header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+		header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
-		$this->db->table('contact_forms')->insert($contact);
+		$contact_server = $_SERVER['HTTP_REFERER'];
+		$contact_site = 'https://www.progalaxyelabs.com/';
+		$contact_local_site = 'http://localhost:4200/';
 
-		//return view('getdata',['contact' => $contact_message]);
+		if($contact_server == $contact_site || $contact_server == $contact_local_site)
+		{
 
-		return redirect()->to('http://localhost:4200/');
+			$data = json_decode(file_get_contents("php://input"));
+
+			$contact_name = filter_var($data->name, FILTER_SANITIZE_STRING);
+			$contact_email = filter_var($data->email, FILTER_SANITIZE_EMAIL);
+			$contact_message = filter_var($data->message, FILTER_SANITIZE_STRING);
+
+			$contact = [
+				'contact_name' => $contact_name,
+				'contact_email' => $contact_email,
+				'contact_message' => $contact_message
+			];
+
+			$this->db->table('contact_forms')->insert($contact);
+
+			return json_encode('Success');
+		}
+		else {
+			return json_encode('Failed');
+		}
 	}
 }
