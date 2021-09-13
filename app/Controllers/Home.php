10548c780e1cc2b0507a549ce3be1cb711822fa0
pradeepdsmk <pradeepdsmk@gmail.com>
2021-09-13 15:20:03 +0530

    CORS failure on progalaxyelabs.com server when submitting form - preflight request OPTIONS is getting a 403 response - exit after sending cors headers if preflight request. also, use http_origin instead of http_referrer.

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,64 +9,61 @@ class Home extends BaseController
 {
 	public function index()
 	{
-
 		header("Access-Control-Allow-Origin: *");
-		header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+		header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 		header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
-		$contact_server = $_SERVER['HTTP_REFERER'];
-		$contact_site = 'https://www.progalaxyelabs.com/';
-		$contact_local_site = 'http://localhost:4200/';
+		if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+			exit(0);
+		}
 
-		if($contact_server == $contact_site || $contact_server == $contact_local_site)
-		{
+		$contact_server = $_SERVER['HTTP_ORIGIN'];
+		$contact_site = 'https://www.progalaxyelabs.com';
+		$contact_local_site = 'http://localhost:4200';
+
+
+		if (($contact_server == $contact_site) || ($contact_server == $contact_local_site)) {
 
 			$data = json_decode(file_get_contents("php://input"));
 
-			if(!empty($data->recaptcha))
-			{
-					$secret = getenv('SECRET_KEY');
-					$verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$data->recaptcha);
-					$responseData = json_decode($verifyResponse);
-					if($responseData->success)
-					{
-						$contact_name = filter_var($data->name, FILTER_SANITIZE_STRING);
-						$contact_email = filter_var($data->email, FILTER_SANITIZE_EMAIL);
-						$contact_message = filter_var($data->message, FILTER_SANITIZE_STRING);
+			if (!empty($data->recaptcha)) {
+				$secret = getenv('SECRET_KEY');
+				$verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $data->recaptcha);
+				$responseData = json_decode($verifyResponse);
+				if ($responseData->success) {
+					$contact_name = filter_var($data->name, FILTER_SANITIZE_STRING);
+					$contact_email = filter_var($data->email, FILTER_SANITIZE_EMAIL);
+					$contact_message = filter_var($data->message, FILTER_SANITIZE_STRING);
 
-						$contact = [
-							'contact_name' => $contact_name,
-							'contact_email' => $contact_email,
-							'contact_message' => $contact_message
-						];
+					$contact = [
+						'contact_name' => $contact_name,
+						'contact_email' => $contact_email,
+						'contact_message' => $contact_message
+					];
 
-						$this->db->table('contact_forms')->insert($contact);
+					$this->db->table('contact_forms')->insert($contact);
 
-						echo json_encode('Success');
-						echo "<br>";
-						echo json_encode("g-recaptcha verified successfully");
-
-					}
-					else
-					{
-						echo json_encode('Failed');
-						echo "<br>";
-						echo json_encode("Some error in verifying g-recaptcha");
-						$this->response->setStatusCode(401);
-					}
-			}
-			else{
+					echo json_encode('Success');
+					echo "<br>";
+					echo json_encode("g-recaptcha verified successfully");
+				} else {
+					echo json_encode('Failed');
+					echo "<br>";
+					echo json_encode("Some error in verifying g-recaptcha");
+					$this->response->setStatusCode(401);
+				}
+			} else {
 				echo json_encode("recaptcha is missing");
+				$this->response->setStatusCode(403);
 			}
-
-			
-		}
-		else {
+		} else {
 			echo json_encode('Failed');
 			$this->response->setStatusCode(403);
 		}
 	}
-	public function get(){
+	
+	public function get()
+	{
 		return view('getdata');
 	}
 }
